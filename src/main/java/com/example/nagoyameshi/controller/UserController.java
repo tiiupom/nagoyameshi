c371a9eeb2510bc77552d7c41d88857d41bd084a
tiiupom <chococo.lav1013.pom@gmail.com>
2025-01-12 02:40:19 +0900

    54

--- src/main/java/com/example/nagoyameshi/controller/UserController.java
+++ src/main/java/com/example/nagoyameshi/controller/UserController.java
@@ -3,18 +3,32 @@ package com.example.nagoyameshi.controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.nagoyameshi.entity.User;
 import com.example.nagoyameshi.form.UserEditForm;
 import com.example.nagoyameshi.security.UserDetailsImpl;
+import com.example.nagoyameshi.service.UserService;
 
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	private final UserService userService;
+	
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	// マイページ
 	@GetMapping
 	public String index(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, Model model) {
 		User user = userDetailsImpl.getUser();
@@ -24,6 +38,7 @@ public class UserController {
 		return "user/index";
 	}
 	
+	// 会員情報
 	@GetMapping("/info")
 	public String info(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, Model model) {
 		User user = userDetailsImpl.getUser();
@@ -33,6 +48,7 @@ public class UserController {
 		return "user/info";
 	}
 	
+	// 会員情報編集を表示
 	@GetMapping("/edit")
 	public String edit(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, Model model) {
 		User user = userDetailsImpl.getUser();
@@ -41,5 +57,33 @@ public class UserController {
 		model.addAttribute("userEditForm", userEditForm);
 		
 		return "user/edit";
+	}
+	
+	// 会員情報編集で入力されたデータのチェック
+	@PostMapping("/update")
+	public String update(@ModelAttribute @Validated UserEditForm userEditForm,
+						 BindingResult bindingResult,
+						 @AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+						 RedirectAttributes redirectAttributes,
+						 Model model)
+	{
+		User user = userDetailsImpl.getUser();
+		
+		// メールアドレスが変更済かつ登録済ならBindingResultオブジェクトにエラー内容を追加
+		if (userService.isEmailChanged(userEditForm, user) && userService.isEmailRegistered(userEditForm.getEmail())) {
+			FieldError fieldError = new FieldError(bindingResult.getObjectName(), "email", "既に登録済みのメールアドレスです");
+			bindingResult.addError(fieldError);
+		}
+		
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("userEditForm", userEditForm);
+			
+			return "user/edit";
+		}
+		
+		userService.updateUser(userEditForm, user);
+		redirectAttributes.addFlashAttribute("successMessage", "会員情報を編集しました");
+		
+		return "redirect:/user/info";
 	}
 }
--- src/test/java/com/example/nagoyameshi/controller/UserControllerTest.java
+++ src/test/java/com/example/nagoyameshi/controller/UserControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.nagoyameshi.controller; 
 
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -10,6 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.nagoyameshi.entity.User;
+import com.example.nagoyameshi.service.UserService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,6 +23,9 @@ import org.springframework.test.web.servlet.MockMvc;
 public class UserControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private UserService userService;
 	
 	@Test
     public void 未ログインの場合は会員用のマイページからログインページにリダイレクトする() throws Exception {
@@ -62,4 +71,37 @@ public class UserControllerTest {
                .andExpect(status().isOk())
                .andExpect(view().name("user/edit"));
     }
+	
+	@Test
+	@Transactional
+	public void 未ログインの場合は更新せずログインページにリダイレクト() throws Exception {
+		mockMvc.perform(post("/user/update")
+				.with(csrf())
+				.param("name", "テスト氏名")
+				.param("furigana", "テストフリガナ")
+				.param("phoneNumber", "000-000-000")
+				.param("email", "test@example.com"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrl("http://localhost/login"));
+	}
+	
+	@Test
+    @WithUserDetails("goro.inoue@example.com")
+    public void ログイン済みの場合は会員情報更新後に会員詳細ページにリダイレクト() throws Exception {
+		User user = userService.findUserByEmail("goro.inoue@example.com");
+		
+		mockMvc.perform(post("/user/update")
+				.with(csrf())
+				.param("name", "テスト氏名")
+				.param("furigana", "テストフリガナ")
+				.param("phoneNumber", "000-000-000")
+				.param("email", "test@example.com"))
+		.andExpect(status().is3xxRedirection())
+		.andExpect(redirectedUrl("/info"));
+		
+		assertThat(user.getName()).isEqualTo("テスト氏名");
+        assertThat(user.getFurigana()).isEqualTo("テストフリガナ");
+        assertThat(user.getPhoneNumber()).isEqualTo("000-000-000");
+        assertThat(user.getEmail()).isEqualTo("test@example.com");
+	}
 }
