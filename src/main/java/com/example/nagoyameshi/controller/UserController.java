48c23c8c89d865819de9831d5a958a2cf9c997af
tiiupom <chococo.lav1013.pom@gmail.com>
2025-01-12 01:32:10 +0900

    51

--- src/main/java/com/example/nagoyameshi/controller/UserController.java
+++ src/main/java/com/example/nagoyameshi/controller/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.nagoyameshi.entity.User;
+import com.example.nagoyameshi.form.UserEditForm;
 import com.example.nagoyameshi.security.UserDetailsImpl;
+
 
 
 @Controller
@@ -29,5 +31,15 @@ public class UserController {
 		model.addAttribute("user", user);
 		
 		return "user/info";
+	}
+	
+	@GetMapping("/edit")
+	public String edit(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, Model model) {
+		User user = userDetailsImpl.getUser();
+		UserEditForm userEditForm = new UserEditForm(user.getName(), user.getFurigana(), user.getPhoneNumber(), user.getEmail());
+				
+		model.addAttribute("userEditForm", userEditForm);
+		
+		return "user/edit";
 	}
 }
