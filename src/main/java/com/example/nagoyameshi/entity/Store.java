83949d70af668e515ac70d1dca361e843a18c48c
tiiupom <chococo.lav1013.pom@gmail.com>
2025-08-01 16:27:25 +0900

    変更

--- src/main/java/com/example/nagoyameshi/entity/Store.java
+++ src/main/java/com/example/nagoyameshi/entity/Store.java
@@ -2,27 +2,20 @@ package com.example.nagoyameshi.entity;
  
 import java.sql.Timestamp;
 import java.time.LocalTime;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import lombok.Data;
-import lombok.ToString;
 
 @Entity
 @Table(name = "stores")
 @Data
-@ToString(exclude = {"reservations", "reviews"})
 public class Store {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,7 +23,7 @@ public class Store {
 	private Integer id;
 	 
 	@ManyToOne
-	@JoinColumn(name = "category_id")
+	@JoinColumn(name = "category")
 	private Category category;
 	
 	@Column(name = "name")
@@ -72,20 +65,4 @@ public class Store {
 	@Column(name = "updated_at", insertable = false, updatable = false)
 	private Timestamp updatedAt;
 	
-	@OneToMany(mappedBy = "store", fetch = FetchType.EAGER, cascade =CascadeType.REMOVE)
-	private List<Reservation> reservations;
-	
-	@OneToMany(mappedBy = "store", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
-    private List<Review> reviews;
-	
-	// 平均評価を取得する
-    @Transient
-    public Double getAverageScore() {
-    	Double averageScore = reviews.stream()
-    								 .mapToInt(Review::getScore)
-    								 .average()
-    								 .orElse(0.0);
-    	
-    	return averageScore;
-    }
 }
